d53b419aa9d4fd6ee7de0ee9dda16f5c4c27eab9
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-01-09 23:05:26 +0600

    authSlice integration

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import authSlice from '../feaatures/auth/authSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authSlice,
+  },
 })
 
 export default store;
